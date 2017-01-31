967bc7b2d4ebe0b5f6e3eef8f3c191f494eb40c1
Thiago Goveia <goveiat@gmail.com>
2017-01-31 15:52:27 -0200

    esqueleto do projeto

---
+++ components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render() {
+        return (
+            <div>ok</div>
+        );
+    }
+
+}
+
+
+App.defaultProps = {
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    }
+}
